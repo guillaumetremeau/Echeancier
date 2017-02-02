895f37bccd765dd4ccdf022dcee50ec20adf0e97
Guillaume Tremeau <tremeauguillaume@gmail.com>
2017-02-02 12:05:49 +0100

    start function createSemaine

--- semaine.c
+++ semaine.c
@@ -1,6 +1,26 @@
+
+
 #define "semaine.h"
-#define <stdlib>
+#define <stdlib.h>
 
-void createSemaine(char * chaine){
+//Procédure qui parcours la liste des semaines et ajoute une semaine (si
+//nécessaire) dans l'ordre en fonction de la chaine de caractère complète (ex:
+//201720114Medecin) puis ajoute l'action correspondant avec la fonction
+//createAction()
+void createSemaine(semaines_t semaines, char * chaine){
+	int[6] semaine;
+	char[13] actions;
+	int cmp=0;
+	}while(chaine[cmp] != '\0');
+	for(cmp=0;cmp<19;cmp++){
+		if(cmp < 6){
+			semaine[cmp]=chaine[cmp];
+		}else action[cmp] = chaine[cmp];
+	}
 
+
+
+	
+
+	createAction(ptrsemaine, actions);
 }
--- semaine.h
+++ semaine.h
@@ -1,13 +1,13 @@
 #define __SEMAINE_H_GUILLAUME___
 #ifndef __SEMAINE_H_GUILLAUME__
 
-typedef struct semaine{
+typedef struct semaines{
 	char[6] semaine;
 	struct actions * actions;
 	struct semaine * semaine;
-}semaine_t;
+}semaines_t;
 
 //Fonction qui créer une semaine à partir d'une chaine de caractère récupérer
 //d'un fichier
-void createSemaine(char *);
+void createSemaine(semaines_t, char *);
 #endif
