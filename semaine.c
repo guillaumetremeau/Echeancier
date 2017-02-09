63a94b2798ec24dea7237a7f5569c23e958841e0
Guillaume Tremeau <tremeauguillaume@gmail.com>
2017-02-09 11:57:38 +0100

    end createSemaine, add alloueSemaine and code recherche in semaine.c

--- semaine.c
+++ semaine.c
@@ -8,36 +8,45 @@
 //nécessaire) dans l'ordre en fonction de la chaine de caractère complète (ex:
 //201720114Medecin) puis ajoute l'action correspondant avec la fonction
 //createAction()
-void createSemaine(semaines_t semaines, char * chaine){
+void createSemaine(semaines_t * semaines, char * chaine){
 	char[6] semaine;
 	char[13] actions;
 	int cmp=0;
-	semaines_t semaineTrouvee;
+	semaines_t * ptrSemaine;
 	
-	for(cmp=0;cmp<19;cmp++){
+	for(cmp=0;cmp<19;cmp++){//Separation de la chaine de caractère
 		if(cmp < 6){
 			semaine[cmp]=chaine[cmp];
-		}else action[cmp] = chaine[cmp];
+		}else action[cmp-6] = chaine[cmp];
 	}
 	
-	semaineTrouvee = recherche(semaines,semaine);
-	if (strcmp(semaineTrouvee.semaineSuivante.semaine, semaine) == 0){
-		semaineTrouvee = semaineTrouvee.semaineSuivante;
-	}else{//allocation
-		
+	ptrSemaine = recherche(semaines,semaine);
+	if ( *ptrSemaine != NULL ){
+		if (strcmp(ptrSemaine->semaine, semaine) != 0){
+			alloueSemaine(ptrSemaine, semaine);
+		}
+	}else{
+			alloueSemaine(ptrSemaine, semaine);
 	}
 	
-	createAction(semaineTrouvee, actions);
+	createAction(ptrSemaine.actions, actions);
 }
 
-//Renvoie l'élément précédant soit l'élément correspondant à la bonne semaine
+//Fonction qui alloue un élément semaine sur le poiteur passé en paramètre et
+//ajoute le champ semaine
+void alloueSemaine(semaines_t * ptrSemaine, char[] nomSemaine){
+	semaines_t * nouvSemaine = (actions_t) malloc(sizeof(actions_t));
+	nouvSemaine->semaine = nomSemaine;
+	nouvSemaine->semaineSuivante = *ptrSemaine;
+	ptrSemaine = nouvSemaine;
+}
+
+//Renvoie le pointeur sur  soit l'élément correspondant à la bonne semaine
 //soit l'emplacement ou inserer un nouvel élément
-semaines_t recherche(semaines_t semaines, char * semaine){
-	
-	semaines_t semaineCourante = semaines;
-	cond = strcmp(semaineCourante.semaine, semaine)
-	while ((semaineCourant != NULL) and (cond < 0)){
-		semaineCourant = semaineCourant.semaineSuivante;
-		cond = strcmp(semaineCourante.semaine, semaine)
+semaines_t * recherche(semaines_t * semaines, char * semaine){
+	semaines_t ** ptrCour = &semaines;
+	while ((*ptrCour != NULL) && (strcmp(*ptrCour.semaine,semaine)<0)){
+		ptrCour = *ptrCour.semaineSuivante;
 	}
+	return ptrCour;
 }
--- semaine.h
+++ semaine.h
@@ -11,16 +11,18 @@ typedef struct semaines{
 
 //Fonction qui créer une semaine à partir d'une chaine de caractère récupérer
 //d'un fichier
-void createSemaine(semaines_t, char *);
+void createSemaine(semaines_t *, char *);
+
+void alloueSemaine(semaines_t *, char[]);
 
 //Fonction qui recherche  un élément dans la sdd
-semaines_t recherche(semaines_t, char *);
+semaines_t * recherche(semaines_t *, char *);
 
 //Fonction qui enregistre la sdd dans un fichier
-void sauvegarde(semaines_t, char *);
+void sauvegarde(semaines_t *, char *);
 
 //Fonction qui créer la liste des jours contenant une action donnée
-char * createListeJourFromAction(semaines_t, char *);
+char * createListeJourFromAction(semaines_t *, char *);
 		
 //Supprime une action de la sdd à partir de l'année, la semaine, le jour
 //ainsi que l'heure
