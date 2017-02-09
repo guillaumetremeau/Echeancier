03fca61244645dc2562a3c31d08a22e89b838dbf
Guillaume Tremeau <tremeauguillaume@gmail.com>
2017-02-09 01:26:07 +0100

    Update semaine.h
    Add functions sauvegarde, createListeJourFromAction, supprimeAction and constant MAXLIST to 50

--- semaine.h
+++ semaine.h
@@ -7,8 +7,23 @@ typedef struct semaines{
 	struct semaine * semaineSuivante;
 }semaines_t;
 
+#define MAXLISTE 50
+
 //Fonction qui créer une semaine à partir d'une chaine de caractère récupérer
 //d'un fichier
 void createSemaine(semaines_t, char *);
-semaines_t recherche(semaines_t semaines, char *);
+
+//Fonction qui recherche  un élément dans la sdd
+semaines_t recherche(semaines_t, char *);
+
+//Fonction qui enregistre la sdd dans un fichier
+void sauvegarde(semaines_t, char *);
+
+//Fonction qui créer la liste des jours contenant une action donnée
+char * createListeJourFromAction(semaines_t, char *);
+		
+//Supprime une action de la sdd à partir de l'année, la semaine, le jour
+//ainsi que l'heure
+void supprimeAction(semaines_t, char *);
+
 #endif
