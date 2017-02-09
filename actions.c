1bedc98049e23ceb805bd978c533163a5e0ca09b
fakouiss <fkouiss@free.fr>
2017-02-09 12:00:53 +0100

    end createActions, add alloueActions,  actions.h et actions.c

--- actions.c
+++ actions.c
@@ -1,29 +1,63 @@
-#define "actions.h"
-#define "semaine.h"
-#define "main.c"
+#include "actions.h"
+#include "semaine.h"
+#include "main.c"
 
-#define TAILLE_LISTE_AGENDA 100
+#define TAILLE_LIrecSTE_AGENDA 100
 
-void createAction(actions_t, char * chaine_actions){
+void createAction(actions_t * liste_actions, char * chaine_actions){
   int i;
-  int j;
   int jour_heure[3];
   char nom_action[10];
+  actions_t * ptrAction;
   
-  for (i=0; i<3; i++){
-    jour_heure[i] = chaine_actions[i];
-  }
-  
-  for (i=3; i<13; i++){
-    for (j=0; i<10; i++){
-      nom_action[j] = chaine_actions[i];
+  for (i=0; i<3; i++){					//on divise la case chaine_actions avec le jour et l'heure d'un côté 
+    if (i<3){						//et le nom de l'action de l'autre
+      jour_heure[i] = chaine_actions[i];
+    }
+    else{
+      nom_action[i-3] = chaine_actions[i];
     }
   }
   
+  ptrAction = Recherche( liste_actions, jour_heure);	//ptrAction représente la case précédente avant laquelle 
+							//il faut insérer la nouvelle action
+  if (* ptrAction != NULL){
+    if (jour_heure != prec->suiv->jour_heure){
+      alloueAction( ptrAction, jour_heure, nom_action);	//on alloue un nouvel élément
+    }
+    else{
+      printf("La plage horaire est deja occupee, vous ne pouvez pas ajouter d'autres elements");    
+    }
+  }
+  else{
+    alloueAction( ptrAction, jour_heure, nom_action);
+  }
   
 }
 
+//fonction qui alloue de la place à la suite du pointeur et qui insère le nouvel élément à la suite
+void alloueAction( actions_t * ptrAction, int [] jour, char [] nom){
+  actions_t *nouv;
+  nouv = (actions_t) * malloc(sizeof(actions_t));
+  chaine_actions.suiv = * ptrAction;
+  ptrAction = chaine_actions;
+}
 
+
+//à terminer, faire attention aux pointeurs, refaire un schéma
+//fonction de recherche 
+//renvoit l'adresse de l'élément précédent où il faut insérer la nouvelle valeur
+actions_t Recherche ( actions_t * tete_liste, int [] val){
+  actions_t ** cour = &tete_liste;
+  
+  while ( *cour != NULL ) && ( cour->suiv->jour_heure != val) && (cour->suiv->jour_heure < val){ //ou strcmp + include "string.h"
+    cour -> suiv;
+  }
+  return cour;
+}
+
+
+//fonction de lecture du fichier
 int lire_fichier(){
   File* fichier = NULL;
   
--- actions.h
+++ actions.h
@@ -4,7 +4,24 @@
 typedef struct actions{
    char[3] jour_heure;
    char[10] nom_action;
-   struct actions * actions;
-}
+   struct suiv * actions;
+} actions_t;
+
+//fonctions du actions.c
+
+//fonction qui cree une action (separation jour/heure et dénomination)
+void createAction(actions_t *, char *);
+
+//fonction qui alloue de la place et insère un nouvel élément
+void alloueAction( actions_t *, int [], char []);
+
+//fonction recherche qui renvoit l'ément précédent à la valeur à insérer ou NULL
+actions_t Recherche ( actions_t *, int []);
+
+//fonction de lecture du fichier
+int lire_fichier();
+
+//fonction de suppression d'une action en connaissant année, semaine,...
 
 #endif
+
